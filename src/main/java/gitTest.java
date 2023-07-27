74a71dd9a87f2a1553dc3b05b5e7514021c15eaa
Tangjl <2973568173@qq.com>
2023-07-27 15:21:24 +0800

    first commit

---
+++ src/main/java/gitTest.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName: gitTest
+ *
+ * @Author 唐佳林
+ * @Create 2023/7/27 15:18
+ * @Version 17.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
